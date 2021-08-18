7e6a0f842869b9d154f337a1837b4ae4ff9857da
nikoguerrero <nclguerrero@gmail.com>
2021-08-18 14:05:47 -0400

    recognizing third node argument when is md file

--- exercises.js
+++ exercises.js
@@ -3,26 +3,13 @@ const filePath = '/dev/SCL017-md-link/devfolder/test.txt';
 const file = fs.readFileSync(filePath, 'utf8');
 console.log(file);
 
+
 const path = require('path');
 const ext = path.extname('/dev/SCL017-md-link/devfolder/test.txt');
 console.log(ext);
 
 const devFolder = '/dev/SCL017-md-link/devfolder/';
-fs.readdir(devFolder, (err, files) => {
-  if (err) {
-    return console.log('no se pudo escanear el directorio: ' + err);
-  }
-  files.forEach(file => {
-    console.log(file);
-  });
-});
-
-const dirPath = path.join(devFolder, 'testFolder');
-console.log(dirPath);
-
-// const testFolder = '/dev/SCL017-md-link/testFolder/';
-// const directoryPath = path.join(devFolder, testFolder);
-// fs.readdir(directoryPath, (err, files) => {
+// fs.readdir(devFolder, (err, files) => {
 //   if (err) {
 //     return console.log('no se pudo escanear el directorio: ' + err);
 //   }
@@ -30,6 +17,18 @@ console.log(dirPath);
 //     console.log(file);
 //   });
 // });
+
+const dirPath = path.join(devFolder, 'testFolder');
+console.log(dirPath);
+
+fs.readdir(dirPath, (err, files) => {
+  if (err) {
+    return console.log('no se pudo escanear el directorio: ' + err);
+  }
+  files.forEach(file => {
+    console.log(file);
+  });
+});
 
 // const {readFile} = require('fs');
 // const fileRead = (path) => {
@@ -47,20 +46,20 @@ console.log(dirPath);
 //   fileRead('exercises.js');
 // });
 
-const {promises: {readFile}} = require('fs');
-// readFile('index.js').then(fileBuffer => {
-//   console.log(fileBuffer.toString());
+// const {promises: {readFile}} = require('fs');
+// // readFile('index.js').then(fileBuffer => {
+// //   console.log(fileBuffer.toString());
+// // }).catch(error => {
+// //   console.error(error.message);
+// //   process.exit(1);
+// // });
+// Promise.all([
+//   readFile('index.js'),
+//   readFile('exercises.js')
+// ]).then(([index, exercises]) => {
+//   console.log(index.toString());
+//   console.log(exercises.toString());
 // }).catch(error => {
 //   console.error(error.message);
 //   process.exit(1);
 // });
-Promise.all([
-  readFile('index.js'),
-  readFile('exercises.js')
-]).then(([index, exercises]) => {
-  console.log(index.toString());
-  console.log(exercises.toString());
-}).catch(error => {
-  console.error(error.message);
-  process.exit(1);
-});
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = () => {
+// module.exports = () => {
   
-};
+// };
+const fs = require('fs');
+const path = require('path');
+
+if (process.argv.length > 2) {
+  // console.log('argumento usuario ' + process.argv[2]);
+  const filePath = process.argv[2];
+  // const file = fs.readFileSync(filePath, 'utf8');
+  // console.log(file);
+  const fileExt = path.extname(filePath).toLowerCase();
+  const mdExt = '.md';
+  if (fileExt === mdExt) {
+    console.log('cargaste un archivo md');
+  } else {
+    console.log('no es un archivo mark down');
+  }
+  process.exit(1);
+}
+
+// const isArgMd = (path) => {
+//   const fileExt = path.extname(process.argv[2]);
+//   const mdExt = '.md';
+//   if (fileExt === mdExt) {
+//     console.log('cargaste un archivo md');
+//   }
+// };
