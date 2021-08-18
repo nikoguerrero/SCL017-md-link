94137b37e0c91b6d2ef981c92fd0ef1b016f5c7d
nikoguerrero <nclguerrero@gmail.com>
2021-08-18 15:46:43 -0400

    reading file if it has md extension

--- index.js
+++ index.js
@@ -4,25 +4,31 @@
 const fs = require('fs');
 const path = require('path');
 
-if (process.argv.length > 2) {
-  // console.log('argumento usuario ' + process.argv[2]);
-  const filePath = process.argv[2];
-  // const file = fs.readFileSync(filePath, 'utf8');
-  // console.log(file);
-  const fileExt = path.extname(filePath).toLowerCase();
-  const mdExt = '.md';
-  if (fileExt === mdExt) {
-    console.log('cargaste un archivo md');
-  } else {
-    console.log('no es un archivo mark down');
+const init = () => {
+  if (process.argv.length > 2) {
+    const filePath = process.argv[2].toLowerCase();
+    if (isArgMdFile(filePath)) {
+      console.log('cargaste un archivo md');
+      readMDContent(filePath);
+    } else {
+      console.log('no es un archivo mark down');
+    }
+    process.exit(1);
   }
-  process.exit(1);
-}
+};
 
-// const isArgMd = (path) => {
-//   const fileExt = path.extname(process.argv[2]);
-//   const mdExt = '.md';
-//   if (fileExt === mdExt) {
-//     console.log('cargaste un archivo md');
-//   }
-// };
+const isArgMdFile = (filePath) => {
+  const fileExt = path.extname(filePath);
+  const mdExt = '.md';
+  return fileExt === mdExt;
+  // if (fileExt === mdExt) {
+  //   console.log('cargaste un archivo md');
+  // }
+};
+
+const readMDContent = (filePath) => {
+  const file = fs.readFileSync(filePath, 'utf8');
+  console.log(file);
+};
+
+init();
