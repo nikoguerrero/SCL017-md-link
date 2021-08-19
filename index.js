6020c775ae31833d7140badde98dc2f48f1c5dad
nikoguerrero <nclguerrero@gmail.com>
2021-08-19 15:21:02 -0400

    added async read directory function

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const { isArgMdFile, isArgDir, readMDContent, findFiles } = require('./lib/md-links')
+const { isArgMdFile, isArgDir, readMDContent, readDir } = require('./lib/md-links')
 
 const init = () => {
   if (process.argv.length > 2) {
     const filePath = process.argv[2].toLowerCase();
     if (isArgMdFile(filePath)) {
-      console.log('cargaste un archivo md');
+      console.log('this is an md file');
       readMDContent(filePath);
     } else if (isArgDir(filePath)) {
-      console.log('es un directorio');
-      findFiles(filePath)
+      console.log('this is a directory');
+      readDir(filePath);
     } else {
-      console.log('no es un archivo válido');
+      console.log('not a valid file or folder');
     }
-    process.exit(1);
   }
 };
 
 init();
-
