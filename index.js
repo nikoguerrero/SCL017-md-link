9746b81625db00f6579f283a9d5f08fb6b397efe
nikoguerrero <nclguerrero@gmail.com>
2021-08-24 11:18:25 -0400

    reading validate and stats arguments

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const init = () => {
   if (process.argv.length > 2) {
     const filePath = process.argv[2].toLowerCase();
     const absolutePath = path.resolve(filePath);
-    console.log(absolutePath);
+    const options = process.argv;
+    let validate = false;
+    let showStats = false;
+
+    for (let index = 3; index < options.length; index++) {
+      if (options[index].toLowerCase() === '--validate') {
+        validate = true;
+      } else if (options[index].toLowerCase() === '--stats') {
+        showStats = true;
+      }
+    }
+    console.log(options);
     try { // exceptions handling
       isDirOrMd(absolutePath);
     } catch (e) {
