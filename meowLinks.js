7e88a273c4f145f588e32690fda8f0ed6e69e367
nikoguerrero <nclguerrero@gmail.com>
2021-08-31 13:36:41 -0400

    Rearranging console message. Resolved #33

--- meowLinks.js
+++ meowLinks.js
@@ -2,53 +2,47 @@ const { meowDownLinks } = require('./lib/meow-down-links');
 const { asciiArt } = require('./bin/asciiArt')
 const path = require('path');
 
-const init = () => {
-  const userArgs = process.argv;
-  if (userArgs.length > 2) {
-    const filePath = userArgs[2].toLowerCase();
-    const absolutePath = path.resolve(filePath);
-    const options = {
-      validate: false,
-      showStats: false
-    };
+const userArgs = process.argv;
+if (userArgs.length > 2) {
+  const filePath = userArgs[2].toLowerCase();
+  const absolutePath = path.resolve(filePath);
+  const options = {
+    validate: false,
+    showStats: false
+  };
 
-    for (let index = 3; index < userArgs.length; index++) {
-      if (userArgs[index].toLowerCase() === '--validate') {
-        options.validate = true;
-      } else if (userArgs[index].toLowerCase() === '--stats') {
-        options.showStats = true;
-      }
+  for (let index = 3; index < userArgs.length; index++) {
+    if (userArgs[index].toLowerCase() === '--validate') {
+      options.validate = true;
+    } else if (userArgs[index].toLowerCase() === '--stats') {
+      options.showStats = true;
     }
-
-    meowDownLinks(absolutePath, options)
-      .then((results) => {
-        console.log('\n');
-        results.forEach(element => {
-          if (!options.validate && !options.showStats) {
-            console.log(element.href, element.text, element.line, element.file);
-          } else if (options.validate && !options.showStats) {
-            console.log(element.href, element.text, element.line, element.file, element.status, element.ok)
-          } else if (!options.validate && options.showStats) {
-            console.log(`Total: ${element.total}, Unique: ${element.unique}`);
-          } else if (options.validate && options.showStats){
-            console.log(`Total: ${element.total}, Unique: ${element.unique}, Broken: ${element.broken}`);
-          } 
-        });
-        // console.log(results);
-        console.log(`
-                  here are your results
-                  meow :3
-              ${asciiArt[Math.floor(Math.random() * asciiArt.length)]}
-        `);
-        console.log('\n');
-      }).catch((error) => {
-        console.log(error.message);
-      });
   }
-};
 
-init();
-
-module.exports = {
-  init
-};
+  meowDownLinks(absolutePath, options)
+    .then((results) => {
+      console.log('\n');
+      results.forEach(element => {
+        const basicInfo = `Href: ${element.href}` + '\n' + `Text: ${element.text}` + '\n' + `Line: ${element.line}` +'\n' + `Path: ${element.file}` + '\n';
+        const basicStats = `Total: ${element.total}` + '\n' + `Unique: ${element.unique}` + '\n';
+        if (!options.validate && !options.showStats) {
+          console.log(basicInfo);
+        } else if (options.validate && !options.showStats) {
+          console.log(basicInfo + `Response status: ${element.status} (${element.ok})` + '\n');
+        } else if (!options.validate && options.showStats) {
+          console.log(basicStats);
+        } else if (options.validate && options.showStats){
+          console.log(basicStats + `Broken: ${element.broken}`);
+        } 
+      });
+      // console.log(results);
+      console.log(`
+            here are your results
+            meow :3
+            ${asciiArt[Math.floor(Math.random() * asciiArt.length)]}
+      `);
+      console.log('\n');
+    }).catch((error) => {
+      console.log(error.message);
+    });
+}
